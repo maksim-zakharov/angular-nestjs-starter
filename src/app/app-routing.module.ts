493eaf837205c85a3d1b64b383610d362ac57106
Maksim Zakharov <max89701@gmail.com>
2020-08-09 00:37:22 +0300

    убрал мелькание после SSR

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {initialNavigation: 'enabled', scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders.component';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { OrdersService } from './orders.service';
+import { NzTableModule } from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [OrdersComponent],
   providers: [OrdersService],
   imports: [
     CommonModule,
-    OrdersRoutingModule
+    OrdersRoutingModule,
+    NzTableModule
   ]
 })
 export class OrdersModule { }
